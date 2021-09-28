14352740d1cee3275021e5269e6c4aa2eb4a51db
Fatima Haseeb <88284051+FatimaHaseeb000@users.noreply.github.com>
2021-09-28 19:42:24 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var personId=document.getElementById('person')
+var sketchId=document.getElementById('sketch')
+var biographyId=document.getElementById('biography')
+var chronologyId=document.getElementById('chronology')
+var bibliogrphyId=document.getElementById('bibliogrphy')
+
+/*function pdisplay(){
+   
+        personId.textContent='personal'
+    
+}
+function sdisplay(){
+    sketchId.textContent='sketch'
+}
+function bdisplay(){
+biographyId.textContent='biography'
+}
+function cdisplay(){
+chronologyId.textContent='chronolgy'
+}
+function bbdisplay(){
+        bibliogrphyId.textContent='bibliography'
+
+}*/
